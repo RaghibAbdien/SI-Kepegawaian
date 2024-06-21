3bc57c60f5ca9e4d421badc2659247cafb761896
RaghibAbdien <frostline27@gmail.com>
2024-06-21 16:45:56 +0700

    Fix: Form AJAX

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -38,7 +38,7 @@
                 </a>
                 <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton" style="min-width: 11rem;">
                     <li>
-                        <form action="{{ route('logout') }}" method="post">
+                        <form id="formToExclude"action="{{ route('logout') }}" method="post">
                             @csrf
                             <button class="dropdown-item" type="submit"><i class="fa-solid fa-right-from-bracket"></i> Logout</button>
                         </form>
